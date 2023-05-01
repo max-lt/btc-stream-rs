d646ec888e6262f5af36d8c57604bd88a8f3cfdf
max-lt <maxime.tissier@gmail.com>
2023-05-01 14:43:19 +0200

    Handle shutdown signals

--- src/relay/main.rs
+++ src/relay/main.rs
@@ -1,3 +1,5 @@
+use tokio::signal::unix::signal;
+use tokio::signal::unix::SignalKind;
 use tokio::sync::broadcast;
 use tokio::sync::mpsc;
 
@@ -28,12 +30,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn_updates_tx, conn_updates_rx) = mpsc::channel(100);
 
     // Start a new thread for receiving messages
-    let t1 = tokio::spawn(listen_bitcoin_events(tx, conn_updates_rx));
+    tokio::spawn(listen_bitcoin_events(tx, conn_updates_rx));
 
     // Start a new thread for listening for WebSocket connections
-    let t2 = tokio::spawn(listen_ws(rx, conn_updates_tx));
+    tokio::spawn(listen_ws(rx, conn_updates_tx));
 
-    tokio::try_join!(t1, t2)?;
+    // Create a signal listener for SIGINT and SIGTERM
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+
+    // Start a new task to listen for termination signals
+    let shutdown = tokio::spawn(async move {
+        tokio::select! {
+            _ = sigint.recv() => {
+                println!("SIGINT received, shutting down...");
+            }
+            _ = sigterm.recv() => {
+                println!("SIGTERM received, shutting down...");
+            }
+        }
+    });
+
+    // Wait for the signal listener to finish
+    shutdown.await?;
 
     Ok(())
 }
